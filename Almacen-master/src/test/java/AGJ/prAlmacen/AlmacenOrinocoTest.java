a5f7bea3b8d91cb1327208719cd254123b9f6ed5
Andres <andresgj@uma.es>
2017-11-09 11:50:25 +0100

    six test

--- Almacen-master/src/test/java/AGJ/prAlmacen/AlmacenOrinocoTest.java
+++ Almacen-master/src/test/java/AGJ/prAlmacen/AlmacenOrinocoTest.java
@@ -1,44 +1,65 @@
 package AGJ.prAlmacen;
 
 import static org.junit.Assert.*;
+import static org.junit.Assert.assertThat;
 import org.hamcrest.core.Every;
 import static org.hamcrest.Matchers.*;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
+import static org.mockito.Mockito.*;
 
 public class AlmacenOrinocoTest {
-  ListaPedidos listaPedidos1 = new ListaPedidos(50);
-  
-  @Before
-  public void setUp() throws Exception {
-	  listaPedidos1 = new ListaPedidos(50); 
-  }
+	ListaPedidos listaPedidos1 = new ListaPedidos(50);
+	IAgenteDistribuidor AgenteD1 = mock(IAgenteDistribuidor.class);
 
-  @After
-  public void tearDown() throws Exception {
-	  listaPedidos1 = null;
-  }
+	@Before
+	public void setUp() throws Exception {
+		listaPedidos1 = new ListaPedidos(50);
+	}
 
-  @Test
-  public void siSeGeneraUnPedidoEnEsperaElEspacioLibreDeLaListaDisminuyeEnUno() {
-	  Pedido pedido1= listaPedidos1.generarPedidoEnEspera();
-	  System.out.println(pedido1.getId());
-  assertEquals(listaPedidos1.maxPedidosTotal()-pedido1.getId(), listaPedidos1.maxPedidosTotal());
-   
-  }
-  
-  @Test
-  public void siSeGeneraUnPedidoEnEsperaYLaListaEstaVaciaSuIdentificadorEsCero() {
-	 Pedido pedido1= listaPedidos1.generarPedidoEnEspera();
-	 listaPedidos1.generarPedidoEnEspera();
-	 assertEquals(pedido1.getId(), 0);
-  }  
-  
-  @Test (expected = ListaPedidosExcepcion.class)
-  public void siSeGeneraUnPedidoEnEsperaYLaListaEstaLlenaSeElevaUnaExcepcion() {
-	 listaPedidos1 = new ListaPedidos(0);
-	  Pedido pedido1= listaPedidos1.generarPedidoEnEspera();
-}
-  
+	@After
+	public void tearDown() throws Exception {
+		listaPedidos1 = null;
+	}
+
+	@Test
+	public void siSeGeneraUnPedidoEnEsperaElEspacioLibreDeLaListaDisminuyeEnUno() {
+		Pedido pedidobase = listaPedidos1.generarPedidoEnEspera();
+		Pedido pedido1 = listaPedidos1.generarPedidoEnEspera();
+		System.out.println(pedido1.getId());
+		assertEquals(listaPedidos1.maxPedidosTotal() - pedido1.getId(), listaPedidos1.maxPedidosTotal() - 1);
+
+	}
+
+	@Test
+	public void siSeGeneraUnPedidoEnEsperaYLaListaEstaVaciaSuIdentificadorEsCero() {
+		Pedido pedido1 = listaPedidos1.generarPedidoEnEspera();
+		listaPedidos1.generarPedidoEnEspera();
+		assertEquals(pedido1.getId(), 0);
+	}
+
+	@Test(expected = ListaPedidosExcepcion.class)
+	public void siSeGeneraUnPedidoEnEsperaYLaListaEstaLlenaSeElevaUnaExcepcion() {
+		listaPedidos1 = new ListaPedidos(0);
+		Pedido pedido1 = listaPedidos1.generarPedidoEnEspera();
+	}
+	@Test
+	public void siSePasaUnPedidoADistribucionYHayAgentesEntoncesElEstadoDelProcesoEsDistribucion() {
+		Pedido pedido1 = listaPedidos1.generarPedidoEnEspera();
+		when(AgenteD1.hayAgenteDisponible()).thenReturn(true);
+		listaPedidos1.pasarPedidoADistribucion(pedido1.getId(), AgenteD1);
+		assertThat(pedido1.getEstado().toString(), containsString("DISTRIBUCION"));
+	}
+	
+	@Test
+	public void siSePasaUnPedidoADistribucionYNoHayAgentesEntoncesElEstadoDelProcesoEsEnEspera() {
+		Pedido pedido1 = listaPedidos1.generarPedidoEnEspera();
+		when(AgenteD1.hayAgenteDisponible()).thenReturn(false);
+		listaPedidos1.pasarPedidoADistribucion(pedido1.getId(), AgenteD1);
+		String ESTADO = pedido1.getEstado().toString();
+		assertEquals("ESPERA", pedido1.getEstado().toString());
+		assertNotEquals("DISTRIBUCION", pedido1.getEstado().toString());
+
+	}
 }
